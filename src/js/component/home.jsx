fb21c403857ccb8a23e50b2ef347325f1d71d230
RichardoTS <ri.tapiasaez@gmail.com>
2023-03-16 14:21:14 +0000

    Project finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,50 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+	
+	const [toDos, setToDos] = useState("");
+	const [newTodoL, setNewTodoL] = useState([]);
+
+	function handleToDosChange(e) {
+		e.preventDefault()
+		setToDos(e.target.value)
+	}
+
+	function handleToDos(e) {
+		e.preventDefault()
+		if (toDos === "") return
+		setNewTodoL([...newTodoL, {id: Date.now(), text: toDos, number: (newTodoL.length + 1) }])
+		e.target.reset()
+	}
+
+	function removeNewTodoL(id) {
+		setNewTodoL(newTodoL.filter((todo) => todo.id !== id))
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row justify-content-center mt-5">
+				<div className="col-4">
+					<h1 className="title">To-Do List</h1>
+					<div className="col">
+						<form onSubmit={handleToDos}>
+							<input type="text" className="form-control mb-2" placeholder={newTodoL.length == 0 ? "There aren't any tasks, please add one" : "What needs to be done"} onChange={handleToDosChange}></input>
+							<ul className="list-group">
+								{newTodoL.map((todo) => (
+									<li className="list-group-item" key={todo.id}> {todo.text}
+										<button type="button" className="close float-end" aria-label="close" onClick={() => removeNewTodoL(todo.id)}><span aria-hidden="true">&times;</span>
+										</button>
+									</li>
+								))}
+							</ul>
+							<div className="col mb-5">
+								<small className="text-white">{newTodoL.length} items left</small>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
